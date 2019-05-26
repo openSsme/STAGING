d93fb2f4df0cba57023b7b38c3fad5428e788f8b
404 <openssme@gmail.com>
2019-05-26 17:37:58 +0300

    fixed identation

--- favorites.php
+++ favorites.php
@@ -7,20 +7,20 @@
     </head>
     <body>
         <main>
-			<div class="wrapper-main">
-				<h1>מועדפים</h1>
-				<section class="section-default">
-					<div class="profile">
-						<?php
-							session_start();
-							require "includes/f.inc.php";
-						?>
-					</div>
-				</section>
-			</div>
+            <div class="wrapper-main">
+                <h1>מועדפים</h1>
+                <section class="section-default">
+                    <div class="profile">
+                        <?php
+                            session_start();
+                            require "includes/f.inc.php";
+                        ?>
+                    </div>
+                </section>
+            </div>
         </main>
     </body>
-	<footer>
+    <footer>
        <small>2019 &copy; Copyright</small>
     </footer>
 </html>
