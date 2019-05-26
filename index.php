a9bb1c66b69a89cba595371a4b3852efac5f998f
404 <openssme@gmail.com>
2019-05-26 17:39:20 +0300

    fixed identation

--- index.php
+++ index.php
@@ -7,23 +7,23 @@
     </head>
     <body>
         <main>
-			<div class="wrapper-main">
-				<h1>ריכוז תוצאות</h1>
-				<section class="section-default">
-					<div class="profile">
-						<?php
-							session_start();
-							require "includes/s.inc.php";
-						?>
-					</div>
-					<div>
-						<a href="favorites.php"><button>המשך</button></a>
-					</div>
-				</section>
-			</div>
+            <div class="wrapper-main">
+                <h1>ריכוז תוצאות</h1>
+                <section class="section-default">
+                    <div class="profile">
+                        <?php
+                            session_start();
+                            require "includes/s.inc.php";
+                        ?>
+                    </div>
+                    <div>
+                        <a href="favorites.php"><button>המשך</button></a>
+                    </div>
+                </section>
+            </div>
         </main>
     </body>
-	<footer>
+    <footer>
        <small>2019 &copy; Copyright</small>
     </footer>
 </html>
--- login.php
+++ login.php
@@ -7,19 +7,19 @@
     </head>
     <body>
         <main>
-			<div class="wrapper-main">
-				<section class="section-default">
-					<h1>התחברות</h1>
-					<form action="includes/login.inc.php" method="post">
-						<input type="text" name="email" placeholder="שם משתמש" autofocus required>
-						<input type="password" name="password" placeholder="סיסמא" required>
-						<button type="submit" name="login-submit">התחבר</button>
-					</form>
-				</section>
-			</div>
+            <div class="wrapper-main">
+                <section class="section-default">
+                    <h1>התחברות</h1>
+                    <form action="includes/login.inc.php" method="post">
+                        <input type="text" name="email" placeholder="שם משתמש" autofocus required>
+                        <input type="password" name="password" placeholder="סיסמא" required>
+                        <button type="submit" name="login-submit">התחבר</button>
+                    </form>
+                </section>
+            </div>
         </main>
     </body>
-	<footer>
+    <footer>
        <small>2019 &copy; Copyright</small>
     </footer>
 </html>
