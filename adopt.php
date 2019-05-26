bef1df61784fe8c7b50377ba869b861657a3df78
404 <openssme@gmail.com>
2019-05-26 18:41:10 +0300

    fixed identation

--- adopt.php
+++ adopt.php
@@ -11,8 +11,8 @@
                 <section class="section-default">
                     <h1>אנו שמחים שמצאת את הכלב המתאים לך!</h1>
                     <h3>מעתה המשך הטיפול יועבר לעמותה בה הכלב נמצא, נציגנו יחזרו אליך תוך 4 ימים לתיאום מפגש היכרות עם בן המשפחה החדש שלך. תמיד תוכל לחזור לפרטי הכלב ע"י לחיצה על כפתור המועדפים.</h3>
-                       <a href="index.php"><button>סיים וחזור לדף התוצאות</button></a>
-                       <a href="favorites.php"><button>מועדפים</button></a>
+                   <a href="index.php"><button>סיים וחזור לדף התוצאות</button></a>
+                   <a href="favorites.php"><button>מועדפים</button></a>
                 </section>
             </div>
         </main>
